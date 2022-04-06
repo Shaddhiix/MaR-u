691ffc84b3c9ff1ce2744f76f4be2e008ee97d75
Shaddhiix <leonard.alexandre17@gmail.com>
2022-04-07 01:48:52 +0200

    Date & Time PickerDialog

--- app/src/main/java/com/example/mareu/activity/AddMeeting.java
+++ app/src/main/java/com/example/mareu/activity/AddMeeting.java
@@ -1,16 +1,68 @@
 package com.example.mareu.activity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.mareu.databinding.ActivityMeetingAddBinding;
 
-import com.example.mareu.R;
+import java.util.Calendar;
 
-public class AddMeeting extends AppCompatActivity {
+public class AddMeeting extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    public DatePickerDialog datePickerDialog;
+    public TimePickerDialog timePickerDialog;
+    ActivityMeetingAddBinding acBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meeting_add);
+
+        ActivityMeetingAddBinding.inflate(getLayoutInflater());
+        View view = acBinding.getRoot();
+        setContentView(view);
+
+        Calendar calendar = Calendar.getInstance();
+
+        //DatePicker
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        acBinding.tvDateMeeting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
+        //TimePicker
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        acBinding.tvTimeMeeting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog.show();
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+        month +=1;
+        String date = dayOfMonth + "/" + month + "/" + year;
+        acBinding.tvDateMeeting.setText(date);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        String time = hourOfDay + ":" + minute;
+        acBinding.tvTimeMeeting.setText(time);
     }
 }
