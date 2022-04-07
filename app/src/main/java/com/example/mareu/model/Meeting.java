5594be7c6ffc1fcd0c4f253dbff452def83ce555
Shaddhiix <leonard.alexandre17@gmail.com>
2022-04-07 19:44:23 +0200

    Model class

--- app/src/main/java/com/example/mareu/model/Meeting.java
+++ app/src/main/java/com/example/mareu/model/Meeting.java
@@ -1,4 +1,64 @@
 package com.example.mareu.model;
 
-public class Meeting {
+import java.io.Serializable;
+
+public class Meeting implements Serializable {
+
+    private String nameMeeting;
+
+    private String nameRoom;
+
+    private String dateMeeting;
+
+    private String timeMeeting;
+
+    private String personList;
+
+    public Meeting(String nameMeeting, String nameRoom, String dateMeeting, String timeMeeting, String personList) {
+        this.nameMeeting = nameMeeting;
+        this.nameRoom = nameRoom;
+        this.dateMeeting = dateMeeting;
+        this.timeMeeting = timeMeeting;
+        this.personList = personList;
+    }
+
+    public String getNameMeeting() {
+        return nameMeeting;
+    }
+
+    public void setNameMeeting(String nameMeeting) {
+        this.nameMeeting = nameMeeting;
+    }
+
+    public String getNameRoom() {
+        return nameRoom;
+    }
+
+    public void setNameRoom(String nameRoom) {
+        this.nameRoom = nameRoom;
+    }
+
+    public String getDateMeeting() {
+        return dateMeeting;
+    }
+
+    public void setDateMeeting(String dateMeeting) {
+        this.dateMeeting = dateMeeting;
+    }
+
+    public String getTimeMeeting() {
+        return timeMeeting;
+    }
+
+    public void setTimeMeeting(String timeMeeting) {
+        this.timeMeeting = timeMeeting;
+    }
+
+    public String getPersonList() {
+        return personList;
+    }
+
+    public void setPersonList(String personList) {
+        this.personList = personList;
+    }
 }
