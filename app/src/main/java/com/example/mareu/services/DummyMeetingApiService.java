7bcc6f030f9c8bd46d5adc9090c4c84a7af0e4a7
Shaddhiix <leonard.alexandre17@gmail.com>
2022-05-23 19:45:21 +0200

    API Service

--- app/src/main/java/com/example/mareu/services/DummyMeetingApiService.java
+++ app/src/main/java/com/example/mareu/services/DummyMeetingApiService.java
@@ -2,22 +2,49 @@ package com.example.mareu.services;
 
 import com.example.mareu.model.Meeting;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class DummyMeetingApiService implements MeetingApiService {
 
-    private List<Meeting> meetings = MeetingGenerator.generateMeetings();
+    private final List<Meeting> meetings = MeetingGenerator.generateMeetings();
 
+    /** Get a Meeting list */
     @Override
     public List<Meeting> getMeeting() {
         return meetings;
     }
 
+    /** Get a Meeting list filtered by Room */
+    @Override
+    public List<Meeting> getMeetingByRoom(String roomSelected) {
+        List<Meeting> filterByRoom = new ArrayList<>();
+        for (Meeting m : meetings) {
+            if (m.getNameRoom().equals(roomSelected)) {
+                filterByRoom.add(m);
+            }
+        }
+        return filterByRoom;
+    }
+
+    /** Get a Meeting list filtered by date */
+    @Override
+    public List<Meeting> getMeetingByDate(Date date) {
+        List<Meeting> filterByDate = new ArrayList<>();
+        for (Meeting m : meetings) {
+
+        }
+        return filterByDate;
+    }
+
+    /** create a Meeting */
     @Override
     public void createMeeting(Meeting meeting) {
         meetings.add(meeting);
     }
 
+    /** Delete a Meeting */
     @Override
     public void deleteMeeting(Meeting meeting) {
         meetings.remove(meeting);
--- app/src/main/java/com/example/mareu/services/MeetingApiService.java
+++ app/src/main/java/com/example/mareu/services/MeetingApiService.java
@@ -2,14 +2,24 @@ package com.example.mareu.services;
 
 import com.example.mareu.model.Meeting;
 
+import java.util.Date;
 import java.util.List;
 
 public interface MeetingApiService {
 
+    /** Get a Meeting list */
     List<Meeting> getMeeting();
 
+    /** Get a Meeting list filtered by Room */
+    List<Meeting> getMeetingByRoom(String roomSelected);
+
+    /** Get a Meeting list filtered by Date */
+    List<Meeting> getMeetingByDate(Date date);
+
+    /** create a Meeting */
     void createMeeting(Meeting meeting);
 
+    /** Delete a Meeting */
     void deleteMeeting(Meeting meeting);
 
 }
