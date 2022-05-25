df7185e093d7c305bf68c4bae775755c688680cb
Shaddhiix <leonard.alexandre17@gmail.com>
2022-05-25 13:04:01 +0200

    Filtrage

--- app/src/main/java/com/example/mareu/events/FilterByRoomEvent.java
+++ app/src/main/java/com/example/mareu/events/FilterByRoomEvent.java
@@ -3,9 +3,10 @@ package com.example.mareu.events;
 public class FilterByRoomEvent {
     String roomSelected;
 
-    public FilterByRoomEvent(String roomSelected){
+    public FilterByRoomEvent(String roomSelected) {
         this.roomSelected = roomSelected;
     }
+
     public String getRoomSelected() {
         return roomSelected;
     }
