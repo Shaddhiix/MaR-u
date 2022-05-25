1573aa7ff730ec786ffe875dffcd17988ad630dc
Shaddhiix <leonard.alexandre17@gmail.com>
2022-05-25 19:05:01 +0200

    Filtrage

--- app/src/main/java/com/example/mareu/dialog_box/DateDialog.java
+++ app/src/main/java/com/example/mareu/dialog_box/DateDialog.java
@@ -1,11 +1,15 @@
 package com.example.mareu.dialog_box;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +23,7 @@ import org.greenrobot.eventbus.EventBus;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateDialog extends DialogFragment {
@@ -53,7 +58,7 @@ public class DateDialog extends DialogFragment {
         dialogDate.dialogDateEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                pickerDate(dialogDate.dialogDateEdit, getActivity());
             }
         });
 
@@ -78,5 +83,35 @@ public class DateDialog extends DialogFragment {
                 dismiss();
             }
         });
+    }
+
+    private void pickerDate(EditText mDayEditText, Context context) {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar cPicked = Calendar.getInstance();
+                cPicked.set(Calendar.YEAR, year);
+                cPicked.set(Calendar.MONTH, month);
+                cPicked.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                String pickedDate = simpleDateFormat.format(cPicked.getTime());
+
+                /*
+                  prevent selecting a date before today
+                */
+                if (cPicked.before(c)) {
+                    mDayEditText.setError("Impossible de choisir une date antérieure à aujourd'hui");
+                } else {
+                    mDayEditText.setText(pickedDate);
+                }
+            }
+        }, year, month, day);
+        datePickerDialog.show();
     }
 }
