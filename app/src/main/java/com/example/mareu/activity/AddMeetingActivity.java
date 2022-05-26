ef1d1d6560f13da4099ed1f44c57d65065d9ae7b
Shaddhiix <leonard.alexandre17@gmail.com>
2022-05-26 12:46:30 +0200

    Add Meeting

--- app/src/main/java/com/example/mareu/activity/AddMeetingActivity.java
+++ app/src/main/java/com/example/mareu/activity/AddMeetingActivity.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
@@ -36,36 +37,36 @@ public class AddMeetingActivity extends AppCompatActivity implements DatePickerD
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        acBinding = ActivityMeetingAddBinding.inflate(getLayoutInflater());
-        setContentView(acBinding.getRoot());
-        meetingApiService = DI.getMeetingApiService();
+        super.onCreate ( savedInstanceState );
+        acBinding = ActivityMeetingAddBinding.inflate ( getLayoutInflater () );
+        setContentView ( acBinding.getRoot () );
+        meetingApiService = DI.getMeetingApiService ();
 
         //Press Back
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar ().setDisplayHomeAsUpEnabled ( true );
 
         //Date & time Picker
-        initListener();
+        initListener ();
 
         //Spinner
-        initSpinner();
+        initSpinner ();
 
         //Chip
-        initChip();
+        initChip ();
 
-        randomColor();
-        addMeeting();
+        randomColor ();
+        addMeeting ();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+        switch (item.getItemId ()) {
             case android.R.id.home: {
-                finish();
+                finish ();
                 return true;
             }
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected ( item );
     }
 
     @Override
@@ -73,14 +74,14 @@ public class AddMeetingActivity extends AppCompatActivity implements DatePickerD
 
         month += 1;
         String date = dayOfMonth + "/" + month + "/" + year;
-        acBinding.tvDateMeeting.setText(date);
+        acBinding.tvDateMeeting.setText ( date );
     }
 
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 
         String time = hourOfDay + "h" + minute;
-        acBinding.tvTimeMeeting.setText(time);
+        acBinding.tvTimeMeeting.setText ( time );
     }
 
     private void addMeeting() {
@@ -94,72 +95,73 @@ public class AddMeetingActivity extends AppCompatActivity implements DatePickerD
                     getChipGroupValues(),
                     color);
 
-            meetingApiService.createMeeting(meeting);
-            finish();
-        });
+            meetingApiService.createMeeting ( meeting );
+            Toast.makeText ( this, "Réunion enregistrée", Toast.LENGTH_SHORT ).show ();
+            finish ();
+        } );
     }
 
     private void randomColor() {
-        Random rnd = new Random();
-        color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-        Drawable view = acBinding.avatarRoom.getBackground();
-        view.setTint(color);
+        Random rnd = new Random ();
+        color = Color.argb ( 255, rnd.nextInt ( 256 ), rnd.nextInt ( 256 ), rnd.nextInt ( 256 ) );
+        Drawable view = acBinding.avatarRoom.getBackground ();
+        view.setTint ( color );
     }
 
     private void initListener() {
 
-        acBinding.tvDateMeeting.setOnClickListener(v -> {
-            DialogFragment datePicker = new DatePickerFragment();
-            datePicker.show(getSupportFragmentManager(), "date picker");
-        });
+        acBinding.tvDateMeeting.setOnClickListener ( v -> {
+            DialogFragment datePicker = new DatePickerFragment ();
+            datePicker.show ( getSupportFragmentManager (), "date picker" );
+        } );
 
-        acBinding.tvTimeMeeting.setOnClickListener(v -> {
-            DialogFragment timePicker = new TimePickerFragment();
-            timePicker.show(getSupportFragmentManager(), "time picker");
-        });
+        acBinding.tvTimeMeeting.setOnClickListener ( v -> {
+            DialogFragment timePicker = new TimePickerFragment ();
+            timePicker.show ( getSupportFragmentManager (), "time picker" );
+        } );
     }
 
     private void initSpinner() {
 
         spinRoom = acBinding.spinnerRoomMeeting;
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.room_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinRoom.setAdapter(adapter);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource ( this,
+                R.array.room_array, android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource ( android.R.layout.simple_spinner_dropdown_item );
+        spinRoom.setAdapter ( adapter );
     }
 
     private void initChip() {
 
-        acBinding.tvPersonName.setOnEditorActionListener((v, actionId, event) -> {
+        acBinding.tvPersonName.setOnEditorActionListener ( (v, actionId, event) -> {
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                String email = acBinding.tvPersonName.getText().toString();
-                if (!email.isEmpty()) {
-                    addChipGroup(email);
-                    acBinding.tvPersonName.setText("");
+                String email = acBinding.tvPersonName.getText ().toString ();
+                if (!email.isEmpty ()) {
+                    addChipGroup ( email );
+                    acBinding.tvPersonName.setText ( "" );
                 }
             }
             return true;
-        });
+        } );
     }
 
     private void addChipGroup(String text) {
-        Chip chip = new Chip(this);
-        chip.setText(text);
-        chip.setCloseIconVisible(true);
-        chip.setOnCloseIconClickListener(v -> acBinding.chipGroupMail.removeView(chip));
+        Chip chip = new Chip ( this );
+        chip.setText ( text );
+        chip.setCloseIconVisible ( true );
+        chip.setOnCloseIconClickListener ( v -> acBinding.chipGroupMail.removeView ( chip ) );
 
-        chip.setClickable(false);
-        chip.setCheckable(false);
-        acBinding.chipGroupMail.addView(chip);
+        chip.setClickable ( false );
+        chip.setCheckable ( false );
+        acBinding.chipGroupMail.addView ( chip );
     }
 
     private String getChipGroupValues() {
         ChipGroup chipGroup = acBinding.chipGroupMail;
-        List<String> participants = new ArrayList<>();
-        for (int i = 0; i < chipGroup.getChildCount(); i++) {
-            Chip chip = (Chip) chipGroup.getChildAt(i);
-            participants.add(chip.getText().toString());
+        List<String> participants = new ArrayList<> ();
+        for (int i = 0; i < chipGroup.getChildCount (); i++) {
+            Chip chip = (Chip) chipGroup.getChildAt ( i );
+            participants.add ( chip.getText ().toString () );
         }
-        return participants.toString().replace("[", "").replace("]", "");
+        return participants.toString ().replace ( "[", "" ).replace ( "]", "" );
     }
 }
